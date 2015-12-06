9f5b5128b1f5d7bc160dd5f7b264ab2a422676f6
liuyidi <928171466@qq.com>
2015-12-06 21:53:53 +0800

    modify plugin 	modified:   jquery.navScrollSpy.js

--- jquery.navScrollSpy.js
+++ jquery.navScrollSpy.js
@@ -31,6 +31,7 @@
     function navScrollSpy(element, options){
         this.element = element;             //获得id=#nav
         this.$ele = $(element);             //获得$("#nav")
+        this.$win = $(window);              //获取window
         this.options = $.extend({}, defaults, options);   //得到参数值
 
         this._defaults = defaults;
@@ -51,7 +52,7 @@
             this.$a.on("click", $.proxy(this.clickSwitch,this));
 
             //滚动切换导航按钮
-            this.scrolling();
+            this.$win.on("scroll",$.proxy(this.scrolling,this));
 
             //当页面重置时会发生问题？
             return this;
@@ -68,31 +69,25 @@
         getBoxTop: function(){
             var self = this;
             self.$a.each(function(){
-                var boxId = self.$a.attr("href").split('#')[1];
+                var boxId = $(this).attr("href").split('#')[1];
                 var boxTop = $("#"+boxId).offset().top;
                 //存放boxtop到box对象中去
                 self.boxs[boxId] = parseInt(boxTop);
             });
-            return self.boxs;
-        },
-
-        //根据滚动的值,比较得出滚轮所在的范围
-        getPosition: function(){
-
         },
 
         //滚动切换
         scrolling: function(){
             var st = $(window).scrollTop();
-            var position = this.getPosition(st);   //得到st所在的范围
-            var $parent = this.$ele.find('a[href="#'+ position+'"]').parent();
-
-            $(window).bind("scroll",function(){
-                if(st >= boxTop - 50){
+            var wH = $(window).height();
+            //临界条件: $("#id").offset.top=$(window).height()+$(window).scrollTop();
+            for(var box in this.boxs){
+                if(st >= this.boxs[box]+parseInt(wH/2)){
+                    var $parent = this.$ele.find('a[href="#'+box+'"]').parent();
                     this.changeNav(this,$parent);
                 }
+            };
 
-            });
         },
 
         //点击切换
